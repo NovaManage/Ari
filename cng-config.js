3a0f5f7effd7446f2c0533ee7278f80cfe54602a
NovaManage <Ari@novamanage.com>
2026-02-05 10:09:16 -0500

    Create cng-config.js
    Login Info

---
+++ cng-config.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: "CarriageRegency",
+    username: "CarriageRegency",
+    password: "Regency1!"
+  },
+  {
+    name: "NovaManagement",
+    username: "NovaManagement",
+    password: "Regency1!"
+  },
+  {
+    name: "office",
+    username: "office@novamanage.com",
+    password: "Regency1!"
+  },
+  {
+    name: "sbmshultas",
+    username: "sbmshultas",
+    password: "Leibish@1"
+  }
+];
